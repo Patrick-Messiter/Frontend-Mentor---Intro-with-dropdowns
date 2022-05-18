94226dbc876b13d2c1742ef75acca18a1204dd93
Patrick Messiter <patrick.messiter@gmail.com>
2022-05-18 20:57:30 +1000

    Fourth Commit - Added Media Queries
    Media Queries added giving functionality for mobile. Further JS logic required for responsive side scroll.

--- Frontend Mentor - Intro with Dropdown/Main/script.js
+++ Frontend Mentor - Intro with Dropdown/Main/script.js
@@ -5,25 +5,31 @@ let featuresContainer = document.querySelector(".featuresContainer");
 
 let featuresArrow = document.getElementById('featuresArrow');
 
+let companyButton = document.querySelector("#companyButton");
+let companyContainer = document.querySelector(".companyContainer");
+let companyArrow = document.getElementById("companyArrow");
+
+let heroImage = document.querySelector(".heroImage");
+
+
+let navToggleButtonImage = document.querySelector("#toggleButtonImage");
+let navBar = document.querySelector('nav');
+
+let navToggleButton = document.querySelector("#navToggleButton");
+
 
 featuresButton.addEventListener('click', () => {
     let featuresValue = window.getComputedStyle(featuresContainer).getPropertyValue("visibility");
     if (featuresValue == "hidden") {
         featuresContainer.style.visibility = "visible";
-        featuresContainer.classList.add("temp");
         featuresArrow.src="images/icon-arrow-up.svg";
     } else if (featuresValue == "visible") {
         featuresContainer.style.visibility = "hidden";
-        featuresContainer.classList.remove("temp");
         featuresArrow.src="images/icon-arrow-down.svg"
     }
 });
 
 /* COMPANY BUTTON SECTION */
-
-let companyButton = document.querySelector("#companyButton");
-let companyContainer = document.querySelector(".companyContainer");
-let companyArrow = document.getElementById("companyArrow")
 
 companyButton.addEventListener('click', () => {
     let companyValue = window.getComputedStyle(companyContainer).getPropertyValue("visibility");
@@ -34,12 +40,38 @@ companyButton.addEventListener('click', () => {
     } else if (companyValue == "visible") {
         companyContainer.style.visibility = "hidden";
         companyContainer.classList.remove("temp");
-        companyArrow.src="images/icon-arrow-down.svg"
+        companyArrow.src="images/icon-arrow-down.svg";
     }
 });
 
-function Limitlist () {
-    if (featuresContainer.classList.contains('temp') && companyContainer.classList.contains('temp')) {
-        /*ADD FUNTIONALITY SO THAT ONLY ONE CONTAINER CAN BE VISIBLE AT A TIME HERE*/
+
+
+function mobileChanges () {
+    if (screen.width <= 800) {
+        heroImage.src="images/image-hero-mobile.png";
+    } else {
+        heroImage.src="images/image-hero-desktop.png";
+        navBar.style.display = "grid";
     }
 }
+
+window.addEventListener('resize', mobileChanges);
+
+
+
+navToggleButton.addEventListener('click', () => {
+    let navBarValue = window.getComputedStyle(navBar).getPropertyValue("display");
+    if (navBarValue == "none" || "grid") {
+        navBar.style.display = "flex";
+        navBarValue = "flex";
+        navToggleButtonImage.src = "images/icon-close-menu.svg";
+    } else {
+        navBar.style.display = "none"; 
+        navBarValue = "none"; 
+        navToggleButtonImage.src = "images/icon-menu.svg";
+    }
+    console.log(`This is the ${navBarValue}`);
+});
+
+
+
